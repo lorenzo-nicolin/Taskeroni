8ef2c5b57fe31ca0d2a2d928b5c25fe3f42f31da
Lorenzo <lorenzogovender152@gmail.com>
2022-09-20 09:25:54 +0200

    initial commit

---
+++ Taskeroni/Taskeroni/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Taskeroni.Models;
+
+namespace Taskeroni.Controllers
+{
+    public class TaskController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public TaskController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Taskeroni/Taskeroni/Models/NewTasks.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskeroni.Models
+{
+    public class NewTasks
+    {
+        [Required]
+        public int id { get; set; }
+
+        public int TaskName { get; set; }
+
+        public int TaskStatus{ get; set; }
+    }
+}
